f893684cd736c8be578951cc7be67f858bbf9a32
RometeRazvan <61833357+RometeRazvan@users.noreply.github.com>
2021-01-11 17:32:26 +0200

    UpdateJobs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from '../src/pages/LoginPage';
 import MainPage from '../src/pages/MainPage'
 import UpdateCVPage from './pages/UpdateVCPage';
+import UpdateJobPage from './pages/UpdateJobPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact={true} path="/" component={LoginPage}></Route>
         <Route exact={true} path="/home" component={MainPage}></Route>
         <Route exact={true} path="/updateCV" component={UpdateCVPage}></Route>
+        <Route exact = {true} path = "/updateJob" component = {UpdateJobPage} />
       </Switch>
     </Router>
   );
